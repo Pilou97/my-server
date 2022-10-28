52aef876c4add85d3de03cb4f32cf49947195ad1
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-28 22:48:38 +0200

    feat/better-docker-image

--- torrent/start.sh
+++ torrent/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt install -y procps
 cd /root
-openvpn --config conf.ovpn --auth-user-pass auth.txt --up /start-downloader.sh --down "/usr/bin/pkill openvpn"
+
+conf=/data/`ls /data | sort -R | tail -1`
+
+openvpn --config $conf --auth-user-pass auth.txt --up /start-downloader.sh --down "/usr/bin/pkill openvpn" --dhcp-option DNS 1.1.1.1
